134aa27c8e33fde0edbcc5d53ae66313f362a2fc
reynaldo <reynaldofbocaling@gmail.com>
2025-10-27 13:58:13 +0800

    d

--- src/utils/googleClient.ts
+++ src/utils/googleClient.ts
@@ -1,9 +1,8 @@
 import { OAuth2Client } from "google-auth-library";
 
 const googleClientId =
-  process.env.GOOGLE_CLIENT_ID ||
   "499708572275-dsql3ijmj6p5314mfl4pov0da835o08g.apps.googleusercontent.com";
-  
+  //
 const client = new OAuth2Client(googleClientId);
 
 export const verifyGoogleToken = async (token: string) => {
