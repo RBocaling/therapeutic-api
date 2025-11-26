eb61750a2510fedf34cd00e60bc5570cde90e721
reynaldo <rbocaling@qmdsi.com>
2025-11-26 21:48:38 +0800

    d

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -142,12 +142,17 @@ export const loginUser = async (
       httpOnly: true,
       secure: true,
       sameSite: "none",
+      domain: ".therapeutic-health.site",
+      path: "/",
     });
     res.cookie("refreshToken", tokens.refreshToken, {
       httpOnly: true,
       secure: true,
       sameSite: "none",
+      domain: ".therapeutic-health.site",
+      path: "/",
     });
+
 
     return user;
   } catch (err: any) {
